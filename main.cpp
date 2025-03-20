c5ecea01581fd82580b210dca175eb908340adf5
Sudeep Prasad <sudeep.rp@gmail.com>
2025-03-20 16:47:51 +0530

    add static analysis

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+void testTshirtSize();
+void testPrintColorMap();
+void testWeatherReport();
+
+int main() {
+    testTshirtSize();
+    testPrintColorMap();
+    testWeatherReport();
+    return 0;
+}
